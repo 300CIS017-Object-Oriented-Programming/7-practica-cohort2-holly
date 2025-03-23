630eaa1c3602af9f22ad4da314af158072acd334
smoncayo24 <smoncayo24@javerianacali.edu.co>
2025-03-23 11:09:30 -0500

    implementacion para venta

--- src/venta.h
+++ src/venta.h
@@ -5,10 +5,24 @@
 #ifndef VENTA_H
 #define VENTA_H
 
-
+#include <vector>
+#include "Producto.h"
+#include "cliente.h"
+using namespace std;
 
 class venta {
+private:
+    int id;
+    cliente* cliente;
+    vector<producto*> productosVendidos;
+    vector<int> cantidades;
+public:
+    venta(int id, class cliente* cliente);
+    ~venta();
 
+    void agregarProductoVendido(producto* producto, int cantidad);
+    float calcularTotal() const;
+    void mostrarDetalleVenta() const;
 };
 
 
